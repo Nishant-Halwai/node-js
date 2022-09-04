00035d85d7c2a264aee6f830dc957a28564bdddc
Nishant-Halwai <nishanthalwai52@gmail.com>
2022-09-04 21:43:39 +0530

    add package colors

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 //create local server
-
+/*
 const http = require("http");
 
 http.createServer((req,res)=>{
     res.write("Create server in local system!");
     res.end();
 }).listen(4500);
+*/
+
+//use colors package
+const colors = require('colors');
+console.log("print data with color".bgMagenta);
